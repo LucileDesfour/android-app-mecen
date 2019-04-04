3ccfe10d4f63112b7f2e47ac82c3f1631acf75fa
dioul296 <mamadou-dioulde.diallo.4@ulaval.ca>
2019-04-04 20:02:54 +0200

    Page login

--- app/src/main/java/ca/ulaval/ima/mecenapp/activites/SearchProjectsActivity.java
+++ app/src/main/java/ca/ulaval/ima/mecenapp/activites/SearchProjectsActivity.java
@@ -1,8 +1,11 @@
 package ca.ulaval.ima.mecenapp.activites;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import ca.ulaval.ima.mecenapp.Connexion;
 import ca.ulaval.ima.mecenapp.R;
 
 public class SearchProjectsActivity extends AppCompatActivity {
@@ -11,6 +14,13 @@ public class SearchProjectsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Connexion connexion=new Connexion();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container,connexion).commit();
+
+
     }
+
+
 }
 
